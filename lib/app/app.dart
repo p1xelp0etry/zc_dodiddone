a565bb8d264a7046c1eda0acd41fdc4648ecd866
p1xelp0etry <vtr197797@gmail.com>
2024-07-31 22:03:03 +0300

    login page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:zc_dodiddone/pages/my_home_page.dart';
-
+import '../pages/login_page.dart';
+import '../theme/theme.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: DoDidDoneTheme.lightTheme,
+      home: const LoginPage(),
     );
   }
 }
+
 
